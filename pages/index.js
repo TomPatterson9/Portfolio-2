54ccc309f82cdd58b8b52e0d8138a9ec1fd9090c
TomPatterson9 <35941859+TomPatterson9@users.noreply.github.com>
2022-07-01 16:19:13 +0100

    added hero, containerblock and rainbowhighlight components

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,21 @@
 import Head from 'next/head';
 import { Navbar } from '../components/Navbar';
-
+import { Hero } from '../components/hero';
+import {ContainerBlock } from '../components/ContainerBlock'
 
 
 export default function Home() {
   return (
 <div>
-  <Head>
-    <title>Tom Patterson</title>
-  </Head>
-  <Navbar />
-  <div className='flex flex-col items-center justify-center w-full'>
-      <h1 className='text-3xl'>Home Page</h1>
 
-      
-    </div>
+<ContainerBlock
+      title="Tom Patterson - Developer, Photographer, Creator"
+      description="This is a template built specifically for my blog - Creating a developer portfolio that gets you a job."
+    >
+      <Hero />
+
+</ContainerBlock>
+
 </div>
   )
 }
